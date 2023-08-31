345820e2561baac161af37ae31ba1ea763bfa121
Lupin Khuvutlu <141078586+lupinteq@users.noreply.github.com>
2023-08-31 10:14:06 +0200

    Import the necessary Python libraries.

---
+++ AI_CareerGuide.py
@@ -0,0 +1,7 @@
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.neighbors import NearestNeighbors
+from karas.models import Sequential
+from keras.layers import Dense
